327a27c9610a209e1bfe15bac6b49331e82a5b38
Valentin Mourtialon <valentinmourtialon@protonmail.com>
2024-07-15 16:28:21 +0200

    Part 10 done: Post CRUD + Profile updates

--- project/blog/views.py
+++ project/blog/views.py
@@ -1,4 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
+from django.views.generic import (
+    ListView,
+    DetailView,
+    CreateView,
+    UpdateView,
+    DeleteView,
+)
 from .models import Post
 
 
@@ -7,6 +15,61 @@ def home(request):
     # The second argument is a string representing the subdirectory inside the
     # templates directory in which our html file lives.
     return render(request, "blog/home.html", context)
+
+
+class PostListView(ListView):
+    model = Post
+    template_name = "blog/home.html"
+    # By default, the list view wants to loop over the variable "object_list"
+    # But we can change that behavior with:
+    context_object_name = "posts"
+    ordering = ["-date_posted"]  # The minus sign = Newest (top) to Oldest (bottom)
+
+
+class PostDetailView(DetailView):
+    # Template = /blog/post_detail.html
+    model = Post
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    # Beware "_form" !
+    # Template = /blog/post_form.html
+    model = Post
+    fields = ["title", "content"]
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user  # The current logged in user
+        return super().form_valid(form)
+
+
+# The inheritance order is important !
+class PostUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    # Beware "_form" !
+    # Template = /blog/post_form.html
+    model = Post
+    fields = ["title", "content"]
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user  # The current logged in user
+        return super().form_valid(form)
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:  # type: ignore
+            return True
+        return False
+
+
+class PostDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    # Template = /blog/post_detail.html
+    model = Post
+    success_url = "/"
+
+    def test_func(self):
+        post = self.get_object()
+        if self.request.user == post.author:  # type: ignore
+            return True
+        return False
 
 
 def about(request):
